6d58c77b09ce59d1fb038bbeb92e34847bbfef3b
Megan Glorioso <meganglorioso2016@gmail.com>
2023-02-21 19:56:44 -0700

    added timer and questions

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,10 @@ function startTimer() {
         time--;
         var timerEl = document.querySelector(".button-timer")
         timerEl.textContent = time;
-
+        if(time <= 0) {
+            alert('Time is up!')
+        }
+        
 
     }, 1000
     )
@@ -32,18 +35,60 @@ startButton.addEventListener("click", function(){
 //check if the user clicked the correct choice
 var questions = [
     {
+        id:1,
         questionText: "What is an array?",
         choices: [
-            "A word", 
-            "A sentence", 
-            "A collection of values", 
-            "A number"
-        ],
-        correctChoice: 2
+            { text: "A word", isCorrect: false },
+            { text: "A sentence", isCorrect: false },
+            { text: "A collection of values", isCorrect: true },
+            { text: "A number", isCorrect: false }
+        ]
+        
+    },
+    {
+        id:2,
+        questionText: "Inside which HTML element do we put the JavaScript?",
+        choices: [
+            { text: "<javascript>", isCorrect: false},
+            { text: "<scripting>", isCorrect: false},
+            { text: "<script>", isCorrect: true},
+            { text: "<js>", isCorrect: false}
+        ]
+    },
+    {
+        id:3,
+        questionText: "How do you write \"Hello World\" in an alert box?",
+        choices: [
+            { text: "alertBox(\"Hello World\");", isCorrect: false},
+            { text: "msg(\"Hello World\");", isCorrect: false},
+            { text: "msgBox(\"Hello World\");", isCorrect: false},
+            { text: "alert(\"Hello World\");", isCorrect: true}
+        ]
+    },
+    {
+        id:4,
+        questionText: "How do you write \"Hello World\" in an alert box?",
+        choices: [
+            { text: "function:myFunction()", isCorrect: false},
+            { text: "function myFunction()", isCorrect: true},
+            { text: "function = myFunction()", isCorrect: false},
+        ]
     }
 
 
 ]
+
+
+var start = true;
+
+function loop(id) {
+
+var result = document.getElementsByClassName(question-box);
+result[0]
+
+}
+
+
 function loadQuestion(i) {
     var questionText = questions[i].questionText
     var questionEl = document.querySelector("#questions")
